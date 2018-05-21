71855f0d9d2fdd560919ef61fed3a3c0ac3c2687
ProfBird <birdb@lanecc.edu>
2018-05-21 16:01:00 -0700

    Added a project with tabbed views

---
+++ PickerDemo/PickerDemo2TabbedScreens/StarWarsModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UIKit;
+
+namespace PickerDemo
+{
+	public class StarWarsModel : UIPickerViewModel
+    {
+		private string[] characterNames = {"Han Solo", "Princess Leia", 
+            "Chewbecca", "Artoo", "C-3PO", "Poe Dameron", "Ray", "Finn"};
+        
+		private ViewController controller;
+        
+        public StarWarsModel(ViewController vc)
+        {
+            controller = vc;
+        }
+
+        // The number returned determines the number of rings in the picker
+        public override nint GetComponentCount(UIPickerView pickerView)
+        {
+            return 1;
+        }
+
+        public override nint GetRowsInComponent(UIPickerView pickerView, nint component)
+        {
+            return characterNames.Length;
+        }
+
+        public override string GetTitle(UIPickerView pickerView, nint row, nint component)
+        {
+            return characterNames[row];
+        }
+
+        public override void Selected(UIPickerView pickerView, nint row, nint component)
+        {
+            controller.DisplayName(characterNames[pickerView.SelectedRowInComponent(0)]);
+        }
+
+        public override nfloat GetComponentWidth(UIPickerView picker, nint component)
+        {
+			// there is only one component, this is it's width setting
+			return 200f;
+        }
+
+        public override nfloat GetRowHeight(UIPickerView picker, nint component)
+        {
+            return 50f;
+        }
+    }
+}
---
+++ PickerDemo/PickerDemo2TabbedScreens/ViewController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UIKit;
+// Written by Brian Bird, 2/8/2016
+// Updated 5/21/2018
+
+namespace PickerDemo
+{
+	public partial class ViewController : UIViewController
+	{
+		// Parameterize constructor is required even though it doesn't 
+        // have any code in it.
+		public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        // Create the model object and assign it to the PickerView's 
+        // Model property
+		public override void ViewDidLoad ()
+		{
+			StarWarsPicker.Model = new StarWarsModel(this);  
+			base.ViewDidLoad ();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+        public void DisplayName(string name)
+        {
+            CharacterLabel.Text = name;
+        }
+
+	}
+}
+
