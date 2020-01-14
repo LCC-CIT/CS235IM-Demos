4831f9d6c87c9500b0b635d92583851eae7ed157
Brian Bird <birdb@lanecc.edu>
2020-01-13 18:28:03 -0800

    Added missing code
    App builds without errors and runs without crashing, but "new game" doesn't start a new game.

--- MathGame/MathGame/ViewController.designer.cs
+++ MathGame/MathGame/ViewController.designer.cs
@@ -13,8 +13,196 @@ namespace MathGame
     [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets ()
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIButton Button01 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIButton Button02 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIButton Button03 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIButton Button04 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIButton Button05 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIButton Button06 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIButton Button07 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIButton Button08 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIButton Button09 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UILabel ColumnLabel01 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UILabel ColumnLabel02 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UILabel ColumnLabel03 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UILabel MessageLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIButton NewGameButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UILabel NumberLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UILabel RowLabel01 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UILabel RowLabel02 { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UILabel RowLabel03 { get; set; }
+
+        [Action("TouchUpInsideGridButton:")]
+        [GeneratedCode("iOS Designer", "1.0")]
+        partial void TouchUpInsideGridButton(UIKit.UIButton sender);
+
+        [Action("TouchUpInsideNewGameButton:")]
+        [GeneratedCode("iOS Designer", "1.0")]
+        partial void TouchUpInsideNewGameButton(UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets()
         {
+            if (Button01 != null)
+            {
+                Button01.Dispose();
+                Button01 = null;
+            }
+
+            if (Button02 != null)
+            {
+                Button02.Dispose();
+                Button02 = null;
+            }
+
+            if (Button03 != null)
+            {
+                Button03.Dispose();
+                Button03 = null;
+            }
+
+            if (Button04 != null)
+            {
+                Button04.Dispose();
+                Button04 = null;
+            }
+
+            if (Button05 != null)
+            {
+                Button05.Dispose();
+                Button05 = null;
+            }
+
+            if (Button06 != null)
+            {
+                Button06.Dispose();
+                Button06 = null;
+            }
+
+            if (Button07 != null)
+            {
+                Button07.Dispose();
+                Button07 = null;
+            }
+
+            if (Button08 != null)
+            {
+                Button08.Dispose();
+                Button08 = null;
+            }
+
+            if (Button09 != null)
+            {
+                Button09.Dispose();
+                Button09 = null;
+            }
+
+            if (ColumnLabel01 != null)
+            {
+                ColumnLabel01.Dispose();
+                ColumnLabel01 = null;
+            }
+
+            if (ColumnLabel02 != null)
+            {
+                ColumnLabel02.Dispose();
+                ColumnLabel02 = null;
+            }
+
+            if (ColumnLabel03 != null)
+            {
+                ColumnLabel03.Dispose();
+                ColumnLabel03 = null;
+            }
+
+            if (MessageLabel != null)
+            {
+                MessageLabel.Dispose();
+                MessageLabel = null;
+            }
+
+            if (NewGameButton != null)
+            {
+                NewGameButton.Dispose();
+                NewGameButton = null;
+            }
+
+            if (NumberLabel != null)
+            {
+                NumberLabel.Dispose();
+                NumberLabel = null;
+            }
+
+            if (RowLabel01 != null)
+            {
+                RowLabel01.Dispose();
+                RowLabel01 = null;
+            }
+
+            if (RowLabel02 != null)
+            {
+                RowLabel02.Dispose();
+                RowLabel02 = null;
+            }
+
+            if (RowLabel03 != null)
+            {
+                RowLabel03.Dispose();
+                RowLabel03 = null;
+            }
         }
     }
 }
+
