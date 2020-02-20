b57bd467016d7674d5789e9f71b63e68ea195ac6
Brian Bird <birdb@lanecc.edu>
2020-02-19 16:52:26 -0800

    New app demo

---
+++ TabbedAppXamDemo/TabbedAppXamDemo/MyTabBarController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UIKit;
+
+namespace TabbedAppXamDemo
+{
+    public class MyTabBarController : UITabBarController
+    {
+
+        UIViewController tab1, tab2, tab3;
+
+        public MyTabBarController()
+        {
+            tab1 = new UIViewController();
+            tab1.Title = "Green";
+            tab1.View.BackgroundColor = UIColor.Green;
+
+            tab2 = new UIViewController();
+            tab2.Title = "Orange";
+            tab2.View.BackgroundColor = UIColor.Orange;
+
+            tab3 = new UIViewController();
+            tab3.Title = "Red";
+            tab3.View.BackgroundColor = UIColor.Red;
+
+            var tabs = new UIViewController[] {
+                tab1, tab2, tab3
+            };
+
+            ViewControllers = tabs;
+        }
+    }
+}
